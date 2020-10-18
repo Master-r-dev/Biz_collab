b60ae0580a600c078feca686f9f5e48b7906a41d
Master-r-dev <71697746+Master-r-dev@users.noreply.github.com>
2020-10-18 19:36:59 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Biz_collab.Startup))]
+namespace Biz_collab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

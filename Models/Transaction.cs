722b9569b2d1ae1e0d52b172de958f9ce1fa9814
Master-r-dev <71697746+Master-r-dev@users.noreply.github.com>
2020-10-27 15:42:01 +0300

    Update Transaction.cs

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,18 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Lab1.Models
+namespace Biz_collab.Models
 {
     public class Transaction
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Amount { get; set; }
-       
-        public int OperationType { get; set; }  /// Вычет или пополнение
-        public string Explanation { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string GroupId { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        public bool OperationType { get; set; }  /// Вычет или пополнение
+        [Required]
+        public string Text { get; set; }
+        [Required]
         public DateTime StartTime { get; set; }
       
     }

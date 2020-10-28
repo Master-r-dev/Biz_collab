a82fe0d3fae1886d2c4816343e1ba586ad226eff
Master-r-dev <71697746+Master-r-dev@users.noreply.github.com>
2020-10-29 01:02:02 +0300

    Add files via upload

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,8 @@ namespace Biz_collab.Models
     public class Client
     {
         public string Id = System.Web.HttpContext.Current.User.Identity.GetUserId();
-        public string Login { get; set; }
+        public string Login= System.Web.HttpContext.Current.User.Identity.GetUserName()
+            .Substring(0, System.Web.HttpContext.Current.User.Identity.GetUserName().LastIndexOf("@") + 1); 
         public ICollection<Group> Groups { get; set; }
 
         public Client()

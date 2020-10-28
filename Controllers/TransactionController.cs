06cef53d6f53fc7b8e8452c6c7139381a5186635
Master-r-dev <71697746+Master-r-dev@users.noreply.github.com>
2020-10-28 16:45:23 +0300

    Add files via upload

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,104 @@
+﻿using Biz_collab.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.Provider;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Biz_collab.Controllers
+{
+    public class TransactionController : Controller
+    {
+        // GET: Transaction
+        public ActionResult Index(string GroupId)
+        {
+            GroupContext db = new GroupContext();
+            string userId = System.Web.HttpContext.Current.User.Identity.GetUserId();
+            string groupId= GroupId;
+            IEnumerable<Transaction> transactions = db.Transactions;
+            ViewBag.Transactions = transactions;
+            return View();
+        }
+
+        // GET: Transaction/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Transaction/Create
+        public ActionResult Create(string userId, string GroupId)
+        {
+            Transaction transaction = new Transaction();
+            transaction.UserId = userId;
+            transaction.GroupId = GroupId;
+
+            return View(transaction);
+        }
+
+        // POST: Transaction/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection col)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Transaction/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Transaction/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Transaction/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Transaction/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

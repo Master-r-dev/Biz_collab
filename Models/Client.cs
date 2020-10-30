afc6ab5c1ce68bde947c3565395d9768bfd55e95
Master-r-dev <71697746+Master-r-dev@users.noreply.github.com>
2020-10-30 19:45:29 +0300

    Add files via upload

---
+++ Models/Client.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+namespace Biz_collab.Models
+{
+    public class Client
+    {
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public Client(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        
+        }
+
+        public void GetIdofUser()
+        {
+            string Id = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+           
+        }
+        public string Id { get; set; }
+
+        public string Login { get; set; }
+        public ICollection<Group> Groups { get; set; }
+
+        public Client()
+        {                     
+               Groups = new List<Group>();            
+        }
+    }
+}

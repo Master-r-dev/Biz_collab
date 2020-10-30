4e5faa2d6234a6e06e7ade54abee8d83ffd53946
Master-r-dev <71697746+Master-r-dev@users.noreply.github.com>
2020-10-30 13:58:39 +0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Biz_collab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

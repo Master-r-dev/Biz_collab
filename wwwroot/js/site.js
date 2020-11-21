ec6f1c233daa34746fcbbc5f3c1da077c66b88a3
itsbyme <deffer00@yandex.ru>
2020-11-21 16:48:26 +0300

    Замена кода внутри страниц с C# на JS, перенос кода на JS в wwwroot

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function ShowCreatGroupSettings() {
+    if (document.getElementById("1").checked)
+        document.getElementById("Settings").style.display = "none";
+    else if (document.getElementById("2").checked)
+        document.getElementById("settings").style.display = "block";
+}
+
+function ShowCreatTransactionSettings() {
+    for (var i in ["1", "2", "3"]) {
+        document.getElementById("input_" + i).type = "hidden";
+    }
+    if (document.getElementById("1").ckecked)
+        document.getElementById("input_1").type = "";
+    else if (document.getElementById("2").checked)
+        document.getElementById("input_2").type = "";
+    else if (document.getElementById("3").checked)
+        document.getElementById("input_3").type = "";
+}

c66178ecbc86e2fcf94706d724d700ea56f6aac7
Master-r-dev <justm8just@gmail.com>
2020-12-22 13:15:32 +0300

    Fix

--- Controllers/GroupsController.cs
+++ Controllers/GroupsController.cs
@@ -115,6 +115,43 @@ namespace Biz_collab.Controllers
             {
                 return View(@group);
             }
+            var amounts = new int[trans.Where(t => t.Status == true).Count()];
+            var dates = new int[trans.Where(t => t.Status == true).Count()][];
+            var total_spent = 0;
+            var total_recieved = 0;
+            int k = 0;
+            foreach (var t in trans.Where(t => t.Status == true).OrderBy(tr => tr.StartTime))
+            {
+                if (t.OperationType == 3)
+                {
+                    amounts[k] = t.Amount;
+                    total_recieved += t.Amount;
+
+                }
+                else
+                {
+                    amounts[k] = -t.Amount;
+                    total_spent += t.Amount;
+                }
+                dates[k++] = new int[] { t.StartTime.Year, t.StartTime.Month, t.StartTime.Day };
+
+            }
+            ViewBag.trans_amounts = JsonSerializer.Serialize(amounts);
+            ViewBag.trans_dates = JsonSerializer.Serialize(dates);
+            ViewBag.total_spent = total_spent;
+            ViewBag.total_recieved = total_recieved;
+
+            var clients = new string[group.Clients.Count()];
+            var clients_trans = new int[group.Clients.Count()];
+            k = 0;
+            foreach (var c in group.Clients)
+            {
+                clients[k] = c.Client.Login;
+                clients_trans[k++] = trans.Where(t => t.ClientId == c.Client.Id && t.Status == true).Count();
+            }
+            ViewBag.clients = JsonSerializer.Serialize(clients);
+            ViewBag.clients_trans = JsonSerializer.Serialize(clients_trans);
+
             ViewBag.Count = trans.Count();
             ViewData["CurrentSort"] = sortOrder;
             if (searchString != null)
@@ -150,39 +187,7 @@ namespace Biz_collab.Controllers
             
             ViewBag.Transactions = await PaginatedList<Transaction>.CreateAsync(trans, pageNumber ?? 1, pageSize);
 
-            var amounts = new int[trans.Where(t=>t.Status==true).Count()];
-            var dates = new int[trans.Where(t => t.Status == true).Count()][];
-            var total_spent = 0;
-            var total_recieved = 0;
-            int k = 0;
-            foreach (var t in trans.Where(t=>t.Status==true).OrderBy(tr => tr.StartTime))
-            {
-                if (t.OperationType == 3) {
-                    amounts[k] = t.Amount;
-                    total_recieved += t.Amount;
-
-                } else {
-                    amounts[k] = -t.Amount;
-                    total_spent += t.Amount;
-                }
-                dates[k++] = new int[] { t.StartTime.Year, t.StartTime.Month, t.StartTime.Day };
-
-            }
-            ViewBag.trans_amounts = JsonSerializer.Serialize(amounts);
-            ViewBag.trans_dates  = JsonSerializer.Serialize(dates);
-            ViewBag.total_spent = total_spent;
-            ViewBag.total_recieved = total_recieved;
-
-            var clients = new string[group.Clients.Count()];
-            var clients_trans = new int[group.Clients.Count()];
-            k = 0;
-            foreach (var c in group.Clients)
-            {
-                clients[k] = c.Client.Login;
-                clients_trans[k++] = trans.Where(t => t.ClientId == c.Client.Id && t.Status == true).Count();
-            }
-            ViewBag.clients = JsonSerializer.Serialize(clients);
-            ViewBag.clients_trans = JsonSerializer.Serialize(clients_trans);
+           
             return View(@group);
         }
         [Authorize]

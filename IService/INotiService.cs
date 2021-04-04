fd8096d0ef02ef6d6d2b48ab8ae5c44935429fb1
MeguMan <dimon200019@gmail.com>
2021-04-04 16:55:22 +0300

    notification template

---
+++ IService/INotiService.cs
@@ -0,0 +1,13 @@
+﻿using Biz_collab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biz_collab.IService
+{
+    public interface INotiService
+    {
+        List<Noti> GetNotifications(int nToUserId, bool bIsGetOnlyUnread);
+    }
+}

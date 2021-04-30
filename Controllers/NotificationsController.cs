021d51e77eb5d0042961405016b4d94a6923e9ed
MeguMan <dimon200019@gmail.com>
2021-04-30 14:30:51 +0300

    fixed error while logging, added put request

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -1,10 +1,13 @@
 ﻿using Biz_collab.IService;
 using Biz_collab.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Biz_collab.Controllers
@@ -28,6 +31,37 @@ namespace Biz_collab.Controllers
             _oNotifications = new List<Notification>();
             _oNotifications = _notiService.GetNotifications(nToUserId, bIsGetOnlyUnread);
             return Json(_oNotifications);
+        }
+        [Route("/notificationSeen")]
+        [HttpPut]
+        public ActionResult Cart()
+        {
+            var bodyStr = "";
+            var req = Request;
+            using (StreamReader reader
+                  = new StreamReader(req.Body, Encoding.UTF8, true, 1024, true))
+            {
+                bodyStr = reader.ReadToEndAsync().Result;
+            }
+            var json = JsonConvert.DeserializeObject<Notification>(bodyStr);
+            //СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА ЛЕХА СЮДА
+            //ниже как было реализовано в старом проекте(работало)
+            /*Thread.Sleep(500);
+            var totalPrice = 0;
+            int lastCustomerId = db.Customers.Max(item => item.customerId);
+            foreach (var value in json.Dat)
+            {
+                totalPrice += value.Price * value.Quantity;
+            }
+            db.Orders.Add(new order { order_time = DateTime.Now, price = totalPrice, customerId = lastCustomerId });
+            db.SaveChanges();
+            int lastOrderId = db.Orders.Max(item => item.orderId);
+            foreach (var value in json.Dat)
+            {
+                db.Mos.Add(new mo { orderId = lastOrderId, menu_itemId = value.Id, order_items_quantity = value.Quantity });
+            }
+            db.SaveChanges();*/
+            return RedirectToAction("/");
         }
     }
 }
